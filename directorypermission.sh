0065b8acedaaac02a6dd3b0640e654424904f828
sreelakshmi-381077 <sreelakshmisuresh381076@gmail.com>
2026-03-30 14:29:33 +0530

    Add files via upload

---
+++ directorypermission.sh
@@ -0,0 +1,14 @@
+                                                                                         
+for file in *; do
+if [[ ! -x "$file" ]]; then
+chmod +x "$file"
+echo "Made $file executable"
+fi
+done
+
+
+
+
+
+
+
